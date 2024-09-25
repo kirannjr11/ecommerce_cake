c2fd29eb0eea7ac5f918a05dcfcafb13fecf74c4
kirannjr11 <ghalekiran03@gmail.com>
2024-09-26 02:18:20 +0900

    created user and role

---
+++ src/main/java/com/example/cake/model/Role.java
@@ -0,0 +1,27 @@
+package com.example.cake.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+
+
+}
