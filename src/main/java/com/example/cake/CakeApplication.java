22ca5fca959935ef0666feac64c08e915d00b8cc
kirannjr11 <ghalekiran03@gmail.com>
2024-09-29 00:19:53 +0900

    updated

--- src/main/java/com/example/cake/CakeApplication.java
+++ src/main/java/com/example/cake/CakeApplication.java
@@ -3,7 +3,7 @@ package com.example.cake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
 public class CakeApplication {
 
 	public static void main(String[] args) {
